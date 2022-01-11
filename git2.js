b60d194a6e9c971ae6f10c3fc979006cc48aed50
Kate5823 <70522517+Kate5823@users.noreply.github.com>
2022-01-11 15:58:27 +0300

    1

---
+++ git2.js
@@ -0,0 +1,9 @@
+// Use this sample to create your own voice commands
+intent('hello world', p => {
+    p.play('(hello|hi there)');
+});
+
+// Use this sample to create your own voice commands
+intent('hello world', p => {
+    p.play('(hello|hi there)');
+});
